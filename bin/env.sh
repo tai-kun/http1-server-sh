e5ebec1b8b977a30b8a1f2aecd40114e7395abe4
tai-kun <taikun.dev@gmail.com>
2023-05-03 15:12:52 +0900

    fix: 🩹 文法ミスを修正

--- bin/env.sh
+++ bin/env.sh
@@ -4,7 +4,7 @@ set -e
 
 SERVERSH_LOG_LEVEL="${SERVERSH_LOG_LEVEL:-info}"
 export SERVERSH_LOG_LEVEL="${SERVERSH_LOG_LEVEL,,}"
-export SERVERSH_LOG_FILE="${SERVERSH_LOG_FILE=-/var/log/serversh.log}"
+export SERVERSH_LOG_FILE="${SERVERSH_LOG_FILE:-/var/log/serversh.log}"
 export SERVERSH_INIT_SCRIPT="${SERVERSH_INIT_SCRIPT:-/etc/serversh/src/init.sh}"
 export SERVERSH_MAIN_SCRIPT="${SERVERSH_MAIN_SCRIPT:-/etc/serversh/src/main.sh}"
 export SERVERSH_PORT_NUMBER="${SERVERSH_PORT_NUMBER:-2980}"
