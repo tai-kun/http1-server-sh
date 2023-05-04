b89fd2118aa59ce35a383af706c89a90912e2eff
tai-kun <taikun.dev@gmail.com>
2023-05-04 11:15:46 +0900

    chore: 🚧 開発用のビルドスクリプトを追加

--- examples/run.sh
+++ examples/run.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ "${1:-}" == 'dev' ]]; then
+    docker buildx build \
+        --tag ghcr.io/tai-kun/http1-server-sh:latest \
+        .
+fi
+
 docker run --rm -it -p 2980:2980 -e SERVERSH_LOG_LEVEL=debug \
     -v "$(pwd)/examples/log:/var/log" \
     -v "$(pwd)/examples/src:/etc/serversh/src" \
